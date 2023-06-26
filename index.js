24fca13c6610cb8375293f3b1165344ae693b1a1
Justas Gražulevičius <91288874+jusgra@users.noreply.github.com>
2023-06-26 16:03:34 +0300

    improved list of guesses

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var guessRange = 100;
 var generatedNumber;
 var currentGuessesArray;
+var toHighArray;
+var toLowArray;
 var numberOfGuesses;
 var userTriesString;
+var userToLowTriesString;
+var userToHighTriesString;
+var userGuess;
 
 // $(".popup-container").toggleClass("popup-container-show");
 // $(".popup-background").toggleClass("popup-background-show");
+
+// $(".tries-text").toggle();
 
 startGame();
 
@@ -17,32 +24,6 @@ $(".popup-button").click(function (e) {
   $(".button-submit").removeAttr("disabled", "");
 });
 
-$(".guess-form").submit(function (e) {
-  e.preventDefault();
-
-  numberOfGuesses += 1;
-
-  var userGuess = $(".guess-input").val();
-  currentGuessesArray.push(userGuess);
-
-  updateGuessesList();
-
-  $(".hint-text").addClass("hint-show");
-
-  if (numberOfGuesses == 1) {
-    userTriesString += userGuess;
-  } else if (numberOfGuesses > 1) {
-    userTriesString += ", " + userGuess;
-  }
-  $(".tries-array").text(userTriesString);
-
-  if (userGuess == generatedNumber) endGame();
-  else if (userGuess > generatedNumber) $(".hint-text").html("too&nbsp;<span class='toHigh'>high</span>");
-  else if (userGuess < generatedNumber) $(".hint-text").html("too&nbsp;<span class='toLow'>low</span>");
-
-  $(".guess-input").val("");
-});
-
 $(".restart-button").click(function (e) {
   e.preventDefault();
 
@@ -51,46 +32,102 @@ $(".restart-button").click(function (e) {
   $(".tries-text").toggle();
 });
 
+$(".guess-form").submit(function (e) {
+  e.preventDefault();
+
+  numberOfGuesses += 1;
+
+  userGuess = $(".guess-input").val();
+  currentGuessesArray.push(userGuess);
+
+  $(".hint-text").addClass("hint-show");
+
+  if (userGuess == generatedNumber) endGame();
+  else if (userGuess > generatedNumber) {
+    $(".hint-text").html("too&nbsp;<span class='to-high'>high</span>");
+    toHighArray.push(userGuess);
+    if (toHighArray.length == 1) {
+      userToHighTriesString += userGuess;
+    } else if (numberOfGuesses > 1) {
+      userToHighTriesString += ", " + userGuess;
+    }
+  } else if (userGuess < generatedNumber) {
+    $(".hint-text").html("too&nbsp;<span class='to-low'>low</span>");
+    toLowArray.push(userGuess);
+    if (toLowArray.length == 1) {
+      userToLowTriesString += userGuess;
+    } else if (numberOfGuesses > 1) {
+      userToLowTriesString += ", " + userGuess;
+    }
+  }
+
+  updateGuessesList();
+
+  // $(".guess-input").val("");
+});
+
 function startGame() {
   generatedNumber = Math.floor(Math.random() * guessRange) + 1;
   currentGuessesArray = [];
+  toLowArray = [];
+  toHighArray = [];
   numberOfGuesses = 0;
   userTriesString = "";
+  userToLowTriesString = "";
+  userToHighTriesString = "";
   console.log("answer is - " + generatedNumber);
 
   $(".tries-one").text("");
   $(".tries-two").text("");
   $(".tries-three").text("");
 
+  $(".tries-range").toggle();
+
   $(".tries-array").text(userTriesString);
 
   $(".guess").val("");
   $(".guess").focus();
+
+  $(".tries-too-high").text("");
+  $(".tries-too-low").text("");
 }
 
 function updateGuessesList() {
   var tryOne = $(".tries-one");
   var tryTwo = $(".tries-two");
   var tryThree = $(".tries-three");
+  var tryHigh = $(".tries-too-high");
+  var tryLow = $(".tries-too-low");
 
-  switch (numberOfGuesses) {
-    case 1:
-      $(".tries-text").toggle();
-      tryOne.addClass("tries-span3");
-      break;
-    case 2:
-      tryOne.removeClass("tries-span3");
-      tryOne.addClass("tries-span2");
-      tryTwo.addClass("tries-span2");
-      break;
-    case 3:
-      tryOne.removeClass("tries-span2");
-      break;
+  if (numberOfGuesses == 1) {
+    $(".tries-text").toggle();
+    $(".tries-range").toggle();
   }
+
+  tryLow.text(userToLowTriesString);
+  tryHigh.text(userToHighTriesString);
 
   tryOne.text(currentGuessesArray[numberOfGuesses - 1]);
   tryTwo.text(currentGuessesArray[numberOfGuesses - 2]);
   tryThree.text(currentGuessesArray[numberOfGuesses - 3]);
+
+  if (numberOfGuesses == 1) {
+    userTriesString += userGuess;
+  } else if (numberOfGuesses > 1) {
+    userTriesString += ", " + userGuess;
+  }
+
+  // if (numberOfGuesses == 1) {
+  //   userToLowTriesString += userGuess;
+  // } else if (numberOfGuesses > 1) {
+  //   userTriesString += ", " + userGuess;
+  // }
+
+  $(".tries-array").text(userTriesString);
+
+  // array1.sort(function (a, b) {
+  //   return a - b;
+  // });
 }
 
 function endGame() {
